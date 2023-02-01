98983e3b0795575899b7cdc301827ab7064ea8f6
Amit Giri <thisisag840@gmail.com>
2023-02-02 01:55:23 +0530

    added emoji

---
+++ src/component/emojipedia.js
@@ -0,0 +1,39 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "😀",
+      name: "Grinning",
+      meaning:
+      "It conveys cheerfulness and joy towards something positive"    },
+    {
+      id: 5,
+      emoji: "😃",
+      name: "Smiley",
+      meaning:
+        "Smiley emoji denotes happiness and positive feelings."
+    }
+  ];
+  
+  export default emojipedia;
+  
